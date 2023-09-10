2f05eec760444a97f586be9db161976235a156a5
Abdul Aziz <114148116+userisaziz@users.noreply.github.com>
2023-09-10 22:39:36 +0530

    update base urls

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,13 @@ connectToMongo();
 const port = 5000 || process.env.PORT;
 var cors = require("cors");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://school-managment-v1-git-master-userisaziz.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json()); //to convert request data to json
 
 // Credential Apis
